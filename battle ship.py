5b343ab50731d337d0a5e6a478b3721d4eced68c
tbonegraves <tristanjgraves@gmail.com>
2014-09-08 18:40:31 -0400

    good job

---
+++ battle ship.py
@@ -0,0 +1,11 @@
+print("hi")
+print ("welcom to balltel ship")
+print ("wich color world you like red or blue")
+color=input ('>')
+if color == 'red':
+   print ("youll be on the sith team and your masksout is darth vader")
+elif color =='blue':
+     print("good luck jedi")
+else:
+    print ("bad")
+ 
